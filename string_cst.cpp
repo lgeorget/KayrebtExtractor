0dff9463750740e84a1da01f28da11d0eebcecd2
Laurent Georget <laurent.georget@supelec.fr>
2015-01-05 16:25:08 +0100

    Make string representation better in dumps

--- string_cst.cpp
+++ string_cst.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <algorithm>
 #include <string>
 #include <gcc-plugin.h>
 #include <tree.h>
@@ -15,6 +16,18 @@ StringCst::StringCst(tree t) : Value(t)
 	const char* content = TREE_STRING_POINTER(t);
 
 	_built_str =  "\"" + std::string(content, length) + "\"";
+	size_t n = _built_str.find('\n');
+	for (; n != std::string::npos ; n = _built_str.find('\n',n)) {
+		_built_str.erase(n,1);
+		_built_str.insert(n,"\\\\n ");
+		n+=4;
+	}
+	n = _built_str.find('"');
+	for (; n != std::string::npos ; n = _built_str.find('"',n)) {
+		_built_str.erase(n,1);
+		_built_str.insert(n,"\\\"");
+		n+=2;
+	}
 }
 
 std::string StringCst::print() const
