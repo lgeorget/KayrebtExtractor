de2b2c839ad851020c5a57756d908f08ffb37a4e
Laurent Georget <laurent.georget@supelec.fr>
2015-01-06 10:59:13 +0100

    Add helper script to extract dot files from dump file

---
+++ clip.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+#
+
+use strict;
+use warnings;
+
+my $function;
+my $output;
+
+while (<>)
+{
+	if (/^Function (\w+)/) 
+	{
+		$function = $1;
+		open($output, ">$function.dot") or warn "Couldn't open output file for $function";
+	}
+	elsif (/digraph d/ ... /^\}$/)
+	{
+		print $output $_ if ($output);
+	}
+}
+0;
