8f55f3184f1e49b8e51a3e1dcd28d63e810a91a3
Laurent Georget <laurent.georget@supelec.fr>
2015-03-24 14:06:08 +0100

    Configurator: Separate definition of inner classes from declaration

--- src/configurator.cpp
+++ src/configurator.cpp
@@ -95,6 +95,10 @@ Configurator::Configurator(std::string config, std::string source) : _catdump(Ca
 				_categoryEnding.push_back(std::regex(it->as<std::string>()));
 			}
 		}
+
+		YAML::Node greedy = generalConf["greedy"];
+		if (greedy.IsScalar() && greedy.as<int>() != 0)
+			_greedymode = true;
 	}
 #ifndef NDEBUG
 	std::cerr << "Categories representations: " << std::endl;
@@ -105,6 +109,9 @@ Configurator::Configurator(std::string config, std::string source) : _catdump(Ca
 
 bool Configurator::mustGraph(std::string functionName)
 {
+	if (_greedymode)
+		return true;
+
 #ifndef NDEBUG
 	std::cerr << "Asking for function " << functionName << std::endl;
 	std::cerr << "List of functions: [";
@@ -114,10 +121,79 @@ bool Configurator::mustGraph(std::string functionName)
 	return std::find(_functions.begin(), _functions.end(), functionName) != _functions.end();
 }
 
-const std::function<std::string(unsigned int)>& Configurator::getCategoryDumper() {
-			return _catdump;
-		}
+const std::function<std::string(unsigned int)>& Configurator::getCategoryDumper()
+{
+	return _catdump;
+}
 
-const std::function<unsigned int(std::string)>& Configurator::getCategorizer() {
-			return _categorizer;
+const std::function<unsigned int(std::string)>& Configurator::getCategorizer()
+{
+	return _categorizer;
+}
+
+Configurator::CategoryDumper::CategoryDumper(const Configurator& parent) : _parent(parent)
+{}
+
+std::string Configurator::CategoryDumper::operator()(unsigned int i)
+{
+	return _parent._categoriesRepresentation.at(i);
+}
+
+Configurator::Categorizer::Categorizer(const Configurator& parent) : _parent(parent)
+{}
+
+unsigned int Configurator::Categorizer::operator()(std::string content)
+{
+#ifndef NDEBUG
+	std::cerr << "CategoryStandalone size: " << _parent._categoryStandalone.size() << std::endl;
+#endif
+	if (_persistent) {
+		bool foundEnding = false;
+		unsigned int i;
+		for (i=0 ; i<_parent._categoryEnding.size() && !foundEnding ; i++) {
+			foundEnding = std::regex_match(content, _parent._categoryEnding[i]);
 		}
+		if (foundEnding) { //end of category region
+			unsigned int oldCat = _currentCategory; // the current node is part of the category
+			_currentCategory = 0; // ...but the next will not
+			_persistent = false;
+			return oldCat;
+		} else { //still in category region
+			return _currentCategory; //no support for nested regions nor standalone categories inside regions
+		}
+	}
+	bool foundBeginning = false;
+	unsigned int i;
+	for (i=0 ; i<_parent._categoryBeginning.size() && !foundBeginning ; i++) {
+		foundBeginning = std::regex_match(content, _parent._categoryBeginning[i].first);
+	}
+	if (foundBeginning) {
+		_currentCategory = _parent._categoryBeginning[i-1].second;
+		_persistent = true;
+		return _currentCategory; //beginning of regions have precedence over standalone nodes
+	}
+
+	bool foundStandalone = false;
+	for (i=0 ; i<_parent._categoryStandalone.size() && !foundStandalone ; i++) {
+#ifndef NDEBUG
+		std::cerr << "Matching \'" << content << "\': ";
+#endif
+		foundStandalone = std::regex_match(content, _parent._categoryStandalone[i].first);
+#ifndef NDEBUG
+		std::cerr << std::boolalpha << foundStandalone << std::endl;
+#endif
+	}
+	if (foundStandalone) {
+		if (_parent._categoriesRepresentation.count(_parent._categoryStandalone[i-1].second) == 0) {
+			std::cerr << "WARNING: discarded undefined category " << _parent._categoryStandalone[i-1].second << std::endl;
+			_currentCategory = 0;
+		} else {
+			_currentCategory = _parent._categoryStandalone[i-1].second;
+		}
+	} else {
+		_currentCategory = 0;
+	}
+
+	return _currentCategory;
+}
+
--- src/configurator.h
+++ src/configurator.h
@@ -13,13 +13,12 @@ class Configurator
 		std::vector<std::pair<std::regex,unsigned int>> _categoryBeginning;
 		std::vector<std::regex> _categoryEnding;
 		std::vector<std::pair<std::regex, unsigned int>> _categoryStandalone;
+		bool _greedymode = false;
 
 		class CategoryDumper {
 			public:
-				CategoryDumper(const Configurator& parent) : _parent(parent) {}
-				std::string operator()(unsigned int i) {
-					return _parent._categoriesRepresentation.at(i);
-				}
+				CategoryDumper(const Configurator& parent);
+				std::string operator()(unsigned int i);
 			private:
 				const Configurator& _parent;
 		};
@@ -32,60 +31,8 @@ class Configurator
 				bool _persistent;
 
 			public:
-				Categorizer(const Configurator& parent) : _parent(parent) {}
-				unsigned int operator()(std::string content) {
-#ifndef NDEBUG
-					std::cerr << "CategoryStandalone size: " << _parent._categoryStandalone.size() << std::endl;
-#endif
-					if (_persistent) {
-						bool foundEnding = false;
-						unsigned int i;
-						for (i=0 ; i<_parent._categoryEnding.size() && !foundEnding ; i++) {
-							foundEnding = std::regex_match(content, _parent._categoryEnding[i]);
-						}
-						if (foundEnding) { //end of category region
-							unsigned int oldCat = _currentCategory; // the current node is part of the category
-							_currentCategory = 0; // ...but the next will not
-							_persistent = false;
-							return oldCat;
-						} else { //still in category region
-							return _currentCategory; //no support for nested regions nor standalone categories inside regions
-						}
-					}
-					bool foundBeginning = false;
-					unsigned int i;
-					for (i=0 ; i<_parent._categoryBeginning.size() && !foundBeginning ; i++) {
-						foundBeginning = std::regex_match(content, _parent._categoryBeginning[i].first);
-					}
-					if (foundBeginning) {
-						_currentCategory = _parent._categoryBeginning[i-1].second;
-						_persistent = true;
-						return _currentCategory; //beginning of regions have precedence over standalone nodes
-					}
-
-					bool foundStandalone = false;
-					for (i=0 ; i<_parent._categoryStandalone.size() && !foundStandalone ; i++) {
-#ifndef NDEBUG
-						std::cerr << "Matching \'" << content << "\': ";
-#endif
-						foundStandalone = std::regex_match(content, _parent._categoryStandalone[i].first);
-#ifndef NDEBUG
-						std::cerr << std::boolalpha << foundStandalone << std::endl;
-#endif
-					}
-					if (foundStandalone) {
-						if (_parent._categoriesRepresentation.count(_parent._categoryStandalone[i-1].second) == 0) {
-							std::cerr << "WARNING: discarded undefined category " << _parent._categoryStandalone[i-1].second << std::endl;
-							_currentCategory = 0;
-						} else {
-							_currentCategory = _parent._categoryStandalone[i-1].second;
-						}
-					} else {
-						_currentCategory = 0;
-					}
-
-					return _currentCategory;
-				}
+				Categorizer(const Configurator& parent);
+				unsigned int operator()(std::string content);
 		};
 		std::function<unsigned int(std::string)> _categorizer;
 
